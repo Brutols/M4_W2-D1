f725ded2de1cd93a59983e68de841aad4b70c6fa
Brutols <dtwo97@gmail.com>
2024-01-09 11:14:47 +0100

    fetch with api key + display in html

---
+++ index.js
@@ -0,0 +1,25 @@
+let fetchPhotos = () => {
+    fetch("https://api.pexels.com/v1/search?query=cats&orientation=square&size=small", {
+        headers: {
+            Authorization: "JwgGcDpWuaQ7APH7CM6sJDan5yL2Idrm8dBCnvD0bfvi5fFdyNrBf02s"
+        }
+    })
+    .then((response) => response.json())
+    .then((photos) => {
+        console.log(photos.photos)
+        let container = document.querySelector(".container")
+        let createCard = (src, title, desc, link) => `<div class="card col-3" style="width: 21rem;">
+        <img src="${src}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text text-truncate">${desc}</p>
+          <a href="${link}" class="btn btn-primary text-truncate" target="_blank">More from ${title}</a>
+        </div>
+      </div>`
+        for (const photo of photos.photos) {
+            container.innerHTML += createCard(photo.src.medium, photo.photographer, photo.alt, photo.photographer_url)
+        }
+    })
+}
+
+window.onload = fetchPhotos()
